c370c8e99bbf746d8551ccf1a5149d6aa2255536
huzaifa Mansoor <huzaifa.mansoor53@gmail.com>
2019-08-26 13:57:43 +0500

    Mock data for (Component Tests)

--- Tests/components/LaunchScreenListTest.js
+++ Tests/components/LaunchScreenListTest.js
@@ -1,13 +1,58 @@
 import 'react-native';
 import React from 'react';
-
+import { shallow } from 'enzyme';
 import { LaunchScreenList  } from '../../App/Components/LaunchScreenList';
-import renderer from 'react-test-renderer';
 
+
+const collections = [
+    {
+      "collection": {
+        "collection_id": 1,
+        "res_count": 30,
+        "image_url": "https://b.zmtcdn.com/data/collections/b53772a204429cb9b42313d6dc22bf3c_1556018415.jpg",
+        "url": "https://www.zomato.com/bhuj/top-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+        "title": "Trending This Week",
+        "description": "Most popular restaurants in town this week",
+        "share_url": "http://www.zoma.to/c-280/1"
+      }
+    },
+    {
+        "collection": {
+          "collection_id": 2,
+          "res_count": 30,
+          "image_url": "https://b.zmtcdn.com/data/collections/b53772a204429cb9b42313d6dc22bf3c_1556018415.jpg",
+          "url": "https://www.zomato.com/bhuj/top-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          "title": "Trending This Week",
+          "description": "Most popular restaurants in town this week",
+          "share_url": "http://www.zoma.to/c-280/1"
+        }
+    },
+    {
+        "collection": {
+          "collection_id": 3,
+          "res_count": 30,
+          "image_url": "https://b.zmtcdn.com/data/collections/b53772a204429cb9b42313d6dc22bf3c_1556018415.jpg",
+          "url": "https://www.zomato.com/bhuj/top-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          "title": "Trending This Week",
+          "description": "Most popular restaurants in town this week",
+          "share_url": "http://www.zoma.to/c-280/1"
+        }
+    },
+
+    {
+        "collection": {
+          "collection_id": 3,
+          "res_count": 30,
+          "image_url": "https://b.zmtcdn.com/data/collections/b53772a204429cb9b42313d6dc22bf3c_1556018415.jpg",
+          "url": "https://www.zomato.com/bhuj/top-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          "title": "Trending This Week",
+          "description": "Most popular restaurants in town this week",
+          "share_url": "http://www.zoma.to/c-280/1"
+        }
+    }
+]
 test('LaunchScreenList Snapshot' , () => {
-    const snap = renderer.create(
-        <LaunchScreenList/>
-    ).toJSON();
+    const snap = shallow(<LaunchScreenList collections={collections} />);
 
     expect(snap).toMatchSnapshot();    
 });
--- Tests/components/SearchScreenListTest.js
+++ Tests/components/SearchScreenListTest.js
@@ -1,13 +1,49 @@
 import 'react-native';
 import React from 'react';
-
+import { shallow } from 'enzyme';
 import { SearchScreenList  } from '../../App/Components/SearchScreenList';
 import renderer from 'react-test-renderer';
 
+
+const searchResult  = [
+    {
+        restaurant : {
+            name: "Test Restaurant 1",
+            id: "18699322",
+            featured_image: "https://b.zmtcdn.com/data/pictures/7/18742897/4076ef7f4b04fd9ffae1162ff055087e.jpg?output-format=webp",
+            average_cost_for_two: 1000,
+            user_rating: {
+                "aggregate_rating": "4.1"
+            }
+        }
+    },
+
+    {
+        restaurant : {
+            name: "Test Restaurant 2",
+            id: "18699322",
+            featured_image: "https://b.zmtcdn.com/data/pictures/7/18742897/4076ef7f4b04fd9ffae1162ff055087e.jpg?output-format=webp",
+            average_cost_for_two: 1000,
+            user_rating: {
+                "aggregate_rating": "4.1"
+            }
+        }
+    },
+
+    {
+        restaurant : {
+            name: "Test Restaurant 3",
+            id: "18699322",
+            featured_image: "https://b.zmtcdn.com/data/pictures/7/18742897/4076ef7f4b04fd9ffae1162ff055087e.jpg?output-format=webp",
+            average_cost_for_two: 1000,
+            user_rating: {
+                "aggregate_rating": "4.1"
+            }
+        }
+    }
+]
 test('SearchScreenList Snapshot' , () => {
-    const snap = renderer.create(
-        <SearchScreenList/>
-    ).toJSON();
+    const snap = shallow(<SearchScreenList searchResult={searchResult} />);
 
     expect(snap).toMatchSnapshot();    
 });
